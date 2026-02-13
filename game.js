9b23747651185066cff18f982a9336956933ccef
Miles Kurth <mkurth9612@student.pps.net>
2026-02-13 13:44:46 -0800

    78

--- game.js
+++ game.js
@@ -61,9 +61,9 @@ var gameArea = {
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateGameArea, 20);
 
-        canvas.addEventListener('click', function(){
-            console.log("canvas was clicked");
-        });
+        // canvas.addEventListener('click', function(){
+        //     console.log("canvas was clicked");
+        // });
     },
     clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this. canvas. height);
